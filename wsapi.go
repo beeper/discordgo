77c26f531489d5691b6f0b73abc0d13360718c89
Skip R <tinyslices@gmail.com>
2025-11-25 11:05:22 -0800

    wsapi: stop logging unknown event data

--- wsapi.go
+++ wsapi.go
@@ -835,7 +835,7 @@ func (s *Session) onEvent(messageType int, message []byte, isOnConnect bool) (*E
 		// Either way, READY events must fire, even with errors.
 		s.handleEvent(e.Type, e.Struct)
 	} else {
-		s.log(LogWarning, "unknown event: Op: %d, Seq: %d, Type: %s, Data: %s", e.Operation, e.Sequence, e.Type, string(e.RawData))
+		s.log(LogWarning, "unknown event: Op: %d, Seq: %d, Type: %s", e.Operation, e.Sequence, e.Type)
 	}
 
 	// For legacy reasons, we send the raw event also, this could be useful for handling unknown events.
